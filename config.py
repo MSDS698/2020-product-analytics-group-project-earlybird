98b93aa1c25e26e32e6b06d088afcf686c3e7cf8
Lin Meng <lmeng24@hotmail.com>
2020-04-10 09:47:54 -0700

    test reset

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY=os.urandom(24)
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'week3.db')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345678@early-bird-test.c99zskgls7ob.us-west-1.rds.amazonaws.com/postgres'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'week3.db')
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345678@early-bird-test.c99zskgls7ob.us-west-1.rds.amazonaws.com/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = True # flask-login uses sessions which require a secret Key
